b70e6d3541df307fb1d5efe5d8db30f3131f4079
RichCode <stanleyogada2000@gmail.com>
2023-07-30 19:56:02 +0100

    Cleanup

--- migrations/1690741702743_TRANSACTIONS:-create-table.ts
+++ migrations/1690741702743_TRANSACTIONS:-create-table.ts
@@ -3,33 +3,6 @@ import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
-/*
-TABLE transactions {
-  id SERIAL [PK]
-  created_at TIMESTAMP [note: 'WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP']
-
-  transactions_number VARCHAR(50) [NOT NULL, UNIQUE]
-  is_deposit BOOLEAN
-  is_owealth BOOLEAN
-  is_success BOOLEAN
-  amount DECIMAL(15, 2) [NOT NULL]
-  charge DECIMAL(15, 2) [NOT NULL]
-
-  account_id INTEGER [ref: > accounts.id, UNIQUE, note: 'ON DELETE CASCADE']
-  other_account_id INTEGER [ref: > accounts.id, UNIQUE, note: 'ON DELETE CASCADE']
-
-   note: '
--- either for accounts or other_accounts
-    CHECK(
-      COALESCE((account_id)::BOOLEAN::INTEGER, 0)
-      +
-      COALESCE((other_account_id)::BOOLEAN::INTEGER, 0)
-      = 1
-    )
-  '
-}
-*/
-
 export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.sql(`
     CREATE TABLE IF NOT EXISTS transactions (
