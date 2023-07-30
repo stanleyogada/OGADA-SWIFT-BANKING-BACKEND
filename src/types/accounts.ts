325337550b81988a1ef33dd9e994c7bb66e96552
RichCode <stanleyogada2000@gmail.com>
2023-07-30 20:07:00 +0100

    Add type

---
+++ src/types/accounts.ts
@@ -0,0 +1,53 @@
+/**
+ * 
+ *
+ * export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+    CREATE TYPE OTHER_ACCOUNT_TYPE AS ENUM ('CASHBACK', 'OWEALTH');
+
+
+    CREATE TABLE IF NOT EXISTS accounts (
+      id SERIAL PRIMARY KEY,
+
+      balance DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
+      account_number VARCHAR(10) NOT NULL UNIQUE,
+      transfer_pin VARCHAR(4) NOT NULL,
+
+      user_id INTEGER NOT NULL UNIQUE REFERENCES users(id) ON DELETE CASCADE
+    );
+
+
+    CREATE TABLE IF NOT EXISTS other_accounts (
+      id SERIAL PRIMARY KEY,
+
+      type OTHER_ACCOUNT_TYPE NOT NULL,
+      balance DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
+
+      account_id INTEGER NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+
+      UNIQUE (type, account_id)
+    );
+  `);
+}
+
+ */
+
+enum EOtherAccountType {
+  CASHBACK = "CASHBACK",
+  OWEALTH = "OWEALTH",
+}
+
+type TAccount = {
+  id: number;
+  balance: number;
+  account_number: string;
+  transfer_pin: string;
+  user_id: number;
+};
+
+type TOtherAccount = {
+  id: number;
+  type: EOtherAccountType;
+  balance: number;
+  account_id: number;
+};
