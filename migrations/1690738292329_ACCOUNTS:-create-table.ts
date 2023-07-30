497131a755daa16dcdb0068cfc105ba57e06fe7c
RichCode <stanleyogada2000@gmail.com>
2023-07-30 19:05:42 +0100

    Add new migration

--- migrations/1690738292329_ACCOUNTS:-create-table.ts
+++ migrations/1690738292329_ACCOUNTS:-create-table.ts
@@ -7,6 +7,7 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
   pgm.sql(`
     CREATE TABLE IF NOT EXISTS accounts (
       id SERIAL PRIMARY KEY,
+      
       balance DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
       account_number VARCHAR(10) NOT NULL UNIQUE,
       transfer_pin VARCHAR(4) NOT NULL,
