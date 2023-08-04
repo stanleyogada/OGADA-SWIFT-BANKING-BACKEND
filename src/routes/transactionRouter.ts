1c452c9c166c2a933364e28331f3bf5343ef0df7
RichCode <stanleyogada2000@gmail.com>
2023-08-04 16:31:59 +0100

    Add boilerplate for transaction routes

---
+++ src/routes/transactionRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {});
+router.get("/:id", (req, res) => {});
+router.get("/bank", (req, res) => {});
+router.get("/in-house", (req, res) => {});
+router.get("/reward", (req, res) => {});
+router.get("/mobile", (req, res) => {});
+
+router.post("/send-money", (req, res) => {});
+
+export default router;
