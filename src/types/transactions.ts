f58a5754a8d23dce6f5d53465dfbf8742220febd
RichCode <stanleyogada2000@gmail.com>
2023-08-04 15:44:49 +0100

    Add boilerplate for transaction entity

---
+++ src/types/transactions.ts
@@ -0,0 +1,58 @@
+import type { EAccountType } from "./accounts";
+
+type TTransaction = {
+  id: number;
+  created_at: Date;
+
+  transactions_number: string;
+  is_deposit: boolean;
+  is_success: boolean;
+  type: EAccountType;
+  amount: number;
+  charge: number;
+
+  account_id: number;
+};
+
+type TTransactionBank = {
+  id: number;
+
+  session_id: string;
+  remark: string;
+  bank_name: string;
+  bank_account_full_name: string;
+  bank_account_number: string;
+
+  transaction_id: number;
+};
+
+type TTransactionInHouse = {
+  id: number;
+
+  remark: string;
+  receiver_account_number: string;
+  sender_account_number: string;
+
+  transaction_id: number;
+};
+
+type TTransactionReward = {
+  id: number;
+
+  type: EAccountType;
+  note: string;
+
+  transaction_id: number;
+};
+
+type TTransactionMobile = {
+  id: number;
+
+  operator: string;
+  phone_number: string;
+  is_airtime: boolean;
+
+  transaction_id: number;
+};
+
+export type { TTransaction, TTransactionBank, TTransactionInHouse, TTransactionReward, TTransactionMobile };
