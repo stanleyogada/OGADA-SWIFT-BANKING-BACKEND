5df4a911d59e9c5b4f2c41745e1c6df6185e2d47
RichCode <stanleyogada2000@gmail.com>
2023-08-04 06:46:51 +0100

    Add new View migration file

---
+++ migrations/1691127656828_USERS-ACCOUNTS-VIEW:create-view.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+    CREATE VIEW "users_accounts" AS
+    SELECT 
+		  users.id AS "user_id", 
+      users.email, 
+      users.phone AS "account_number",
+
+      accounts.id AS "account_id",
+      accounts.balance,
+      accounts.type
+    FROM
+      "users"
+    LEFT JOIN 
+      "accounts" ON accounts.user_id = users.id;
+  `);
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.sql(`
+    DROP VIEW "users_accounts";
+  `);
+}
