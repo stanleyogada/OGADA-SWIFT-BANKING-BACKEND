d949b078ddfa9b9980dac964337c6f02b6615491
RichCode <stanleyogada2000@gmail.com>
2023-08-15 08:08:37 +0100

    Cleanup comments

--- src/routes/transactionRouter.ts
+++ src/routes/transactionRouter.ts
@@ -12,11 +12,6 @@ import { TRANSACTIONS_ROUTES } from "../constants/routes";
 
 const router = Router();
 
-// router.get("/", (req, res) => {});
-// router.get("/:id", (req, res) => {});
-// router.get("/reward", (req, res) => {});
-// router.get("/mobile", (req, res) => {});
-
 router.get(TRANSACTIONS_ROUTES.banks, handleAuthGuardRoute, getTransactionsBank);
 router.post(TRANSACTIONS_ROUTES.banksSendMoney, handleAuthGuardRoute, handleValidateTransferPin, sendMoneyBank);
 
