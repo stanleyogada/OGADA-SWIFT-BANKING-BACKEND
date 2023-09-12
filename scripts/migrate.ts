f0a3903cd2c95d1412ea3b0642135cbd79af32d0
RichCode <stanleyogada2000@gmail.com>
2023-09-12 12:43:06 +0100

    ft-#77: Add drop-all migrate script

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -3,6 +3,7 @@ require("dotenv").config();
 import migrate from "node-pg-migrate";
 import { TConnectOpts } from "../src/types/db";
 import getDBConnection from "../src/utils/getDBConnection";
+import pool from "../src/utils/pool";
 
 const DEFAULT_USER_OPTS: TConnectOpts = getDBConnection();
 
@@ -13,21 +14,37 @@ const DEFAULT_USER_OPTS: TConnectOpts = getDBConnection();
     const direction = (() => {
       if (flags.includes("up")) return "up";
       if (flags.includes("down")) return "down";
+      if (flags.includes("down-all")) return "down-all";
 
-      throw new Error('Please specify "up" or "down"');
+      throw new Error('Please specify "up" OR "down" OR "down-all"');
     })();
 
-    const results = await migrate({
-      dir: "migrations",
-      direction,
-      migrationsTable: "pgmigrations",
-      log: (msg: string) => {
-        console.log("Migration message", msg);
-      },
-      databaseUrl: DEFAULT_USER_OPTS,
-    });
+    const runMigration = async () => {
+      const results = await migrate({
+        dir: "migrations",
+        direction: direction.replace("-all", "") as "up" | "down",
+        migrationsTable: "pgmigrations",
+        log: (msg: string) => {
+          console.log("Migration message", msg);
+        },
+        databaseUrl: DEFAULT_USER_OPTS,
+      });
 
-    console.log("Migration complete", results);
+      console.log("Migration complete", results);
+    };
+
+    if (direction === "down-all") {
+      await pool.connect(DEFAULT_USER_OPTS);
+      const { rows } = await pool.query("SELECT * FROM pgmigrations;");
+
+      for (const row of rows) {
+        await runMigration();
+      }
+
+      return;
+    }
+
+    await runMigration();
   } catch (err) {
     console.error("Error running migration", err, "Error running migration");
   }
